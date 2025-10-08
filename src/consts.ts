165e3e42b7e4d87e973d67ec79749ed7a3f3cf80
bando9 <bandomega1@gmail.com>
2025-10-08 22:18:26 +0700

    chore(migration): update index.astro layout; adjust components (WIP); add images to src/public; fix astro.config.mjs links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Bando Mega Kusuma | Software Engineer & Frontend Developer";
+export const SITE_DESCRIPTION =
+  "I'm Bando Mega Kusuma, a Software Engineer and Frontend Developer. Explore my portfolio, projects, blog, and ways to connect with me.";
