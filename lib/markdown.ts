68dc48c20326cc3c971b70c5feb606a404fe3b0f
bando9 <bandomega1@gmail.com>
2025-11-29 23:05:58 +0700

    refactor: change prismjs to @shikijs/markdown-it

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,29 +1,15 @@
 import MarkdownIt from "markdown-it";
-import prism from "markdown-it-prism";
-
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-markdown";
-import "prismjs/components/prism-diff";
-
-function rawCodePlugin(md: MarkdownIt) {
-  const orig = md.renderer.rules.fence!;
-
-  md.renderer.rules.fence = (tokens, idx, options, env, self) => {
-    const token = tokens[idx];
-
-    const raw = encodeURIComponent(token.content);
-
-    const rendered = orig(tokens, idx, options, env, self);
-
-    return rendered.replace("<pre", `<pre data-raw="${raw}"`);
-  };
-}
+import Shiki from "@shikijs/markdown-it";
 
 export const md = new MarkdownIt({
   html: false,
   linkify: true,
   typographer: true,
-})
-  .use(prism)
-  .use(rawCodePlugin);
+}).use(
+  await Shiki({
+    themes: {
+      light: "github-dark-default",
+      dark: "github-dark-default",
+    },
+  })
+);
